5348871b468eeed959bd79a548e8595d7d7b51f8
Denisrrr <denisrianov@gmail.com>
2023-04-06 20:28:13 +0500

    3.12

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import EventsModel from './model/events-model.js';
 import MenuView from './view/menu-view.js';
 import FilterView from './view/filter-view.js';
 import { render, RenderPosition } from './render.js';
-import SortingView from './view/sorting-view';
 import TripInfoView from './view/trip-info-view';
 import RootPresenter from './presenter/root-presenter.js';
+import AddFormView from './view/add-form-view';
 
 const headerElement = document.querySelector('.page-header');
 const mainElement = document.querySelector('.page-main');
@@ -13,6 +13,8 @@ const tripMainElement = document.querySelector('.trip-main');
 const navigation = headerElement.querySelector('.trip-controls__navigation');
 const filters = headerElement.querySelector('.trip-controls__filters');
 const content = mainElement.querySelector('.trip-events');
+tripMainElement.querySelector('.trip-main__event-add-btn')
+  .addEventListener('click', () => render(new AddFormView(), content, RenderPosition.AFTERBEGIN));
 
 const routePresenter = new RootPresenter();
 const eventsModel = new EventsModel();
@@ -20,6 +22,4 @@ const eventsModel = new EventsModel();
 render(new MenuView(), navigation);
 render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filters);
-render(new SortingView(), content);
-
 routePresenter.init(content, eventsModel);
